87f59f334d10c047d313025a24511888feab9893
Rafael Ramos <rafaelramos1028@gmail.com>
2021-06-01 23:44:31 -0500

    aprendiendo a trabajar con ramas

--- prueba.py
+++ prueba.py
@@ -1,2 +1,12 @@
 print("Hola mundo")
 print("\nEsto es una prueba")
+
+#vamos a crear una lista con los números del 1 al 10
+numeros = [0 for x in range (1, 11)]
+
+for i in range (len(numeros)):
+    numeros[i] = i+1
+
+print("\nNumeros del 1 al 10")
+print(numeros)
+
