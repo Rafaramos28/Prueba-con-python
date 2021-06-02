da50ef5f66b3dcee7e64d1f03a785cddcc624152
Rafael Ramos <rafaelramos1028@gmail.com>
2021-06-01 23:57:13 -0500

    Rama sumatoria

--- prueba.py
+++ prueba.py
@@ -3,10 +3,14 @@ print("\nEsto es una prueba")
 
 #vamos a crear una lista con los números del 1 al 10
 numeros = [0 for x in range (1, 11)]
-
 for i in range (len(numeros)):
     numeros[i] = i+1
-
 print("\nNumeros del 1 al 10")
 print(numeros)
 
+#sumatoria
+suma = 0
+for i in range(len(numeros)):
+    suma += numeros[i]
+print("\nsumatoria")
+print(suma)
